bb40ecd94d313479b690f6503891d0671b46471d
ahmedaboslah <aahmedabosalah2@gmail.com>
2024-09-13 19:23:32 +0200

    Category Gridview Done

--- lib/features/Home/presentation/view/widgets/home_view_body.dart
+++ lib/features/Home/presentation/view/widgets/home_view_body.dart
@@ -9,26 +9,36 @@ class HomePageViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Column(
-      crossAxisAlignment:CrossAxisAlignment.start ,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const SizedBox(height: 40,),
+        const SizedBox(
+          height: 40,
+        ),
         const Padding(
           padding: EdgeInsets.only(left: 8),
-          child: Text('Choose your dish',style: TextStyles.semiBold20,),
-          
+          child: Text(
+            'Choose your dish',
+            style: TextStyles.semiBold20,
+          ),
         ),
-        const SizedBox(height: 30,),
+        const SizedBox(
+          height: 30,
+        ),
         Expanded(
-          child: ListView.builder(
-            itemCount: 6,
-            itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10,horizontal: 100),
-              child: CategoryCard(),
+            child: GridView.builder(
+              itemCount: 6,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 2 / 3,
+              mainAxisSpacing: 10),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal:  16,vertical: 8),
+              child: const CategoryCard(),
             );
-          },),
-        )
+          },
+        ))
       ],
     );
   }
